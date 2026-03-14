b4af46780704b330d1e1f5932c14a5cccc4c06ab
DEEPAK-PC <dpakverma789@gmail.com>
2026-03-14 16:13:44 +0530

    fix

--- fixcart_app/views.py
+++ fixcart_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import RepairRequest
 
 def home(request):
     return render(request, 'fixcart_app/index.html')
@@ -7,5 +7,18 @@ def home(request):
 def about(request):
     return render(request, 'fixcart_app/about.html')
 
+
 def contact(request):
-    return render(request, 'fixcart_app/contact.html')
+    if request.method == "POST":
+        RepairRequest.objects.create(
+            name=request.POST.get("name"),
+            category=request.POST.get("category"),
+            problem_description=request.POST.get("problem_description"),
+            pickup_address=request.POST.get("pickup_address"),
+            contact_number=request.POST.get("contact_number"),
+            pickup_map_location=request.POST.get("pickup_map_location"),
+        )
+
+        return redirect("contact")
+
+    return render(request, "fixcart_app/contact.html")
