4ce9b6562f002c674156ed52fbea6fb8428af6b2
DEEPAK-PC <dpakverma789@gmail.com>
2026-03-14 15:51:00 +0530

    fix

--- fixcart_app/admin.py
+++ fixcart_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import RepairRequest
 
-# Register your models here.
+
+@admin.register(RepairRequest)
+class RepairRequestAdmin(admin.ModelAdmin):
+
+    list_display = ("name","category","contact_number","created_at")
+    search_fields = ("name", "contact_number")
+    list_filter = ("category", "created_at")
--- fixcart_app/models.py
+++ fixcart_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class RepairRequest(models.Model):
+
+    name = models.CharField(max_length=150)
+    category = models.CharField(max_length=20,choices=[("mobile", "Mobile"),("laptop", "Laptop"),("other", "Other")])
+    problem_description = models.TextField()
+    pickup_address = models.TextField()
+    contact_number = models.CharField(max_length=15)
+    pickup_map_location = models.URLField(blank=True,null=True,help_text="Paste Google Maps location link")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20,choices=[("pending", "Pending"),("picked", "Picked"),
+            ("repairing", "Repairing"),("completed", "Completed")],default="pending")
+
+    def __str__(self):
+        return f"{self.name} - {self.category}"
