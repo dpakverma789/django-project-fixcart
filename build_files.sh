903a23535945457d6e6691826b15eab4dd0c87c7
DEEPAK-PC <dpakverma789@gmail.com>
2026-03-16 13:47:41 +0530

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,11 @@
 echo "======> INSTALLING REQUIREMENTS <======"
-pip install -r requirements.txt --break-system-packages
+pip install -r requirements.txt
+echo "======> REQUIREMENTS INSTALLED <======"
 
 echo "======> COLLECTING STATIC FILES <======"
-python3.12 manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput --clear
+echo "======> STATIC FILES COLLECTED <======"
 
-echo "======> APPLYING MIGRATIONS <======"
+echo "======> MAKE-MIGRATIONS <======"
 python3.12 manage.py migrate --noinput
+echo "======> MAKE-MIGRATIONS-END <======"
