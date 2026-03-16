555b5cff9e4e72813a3939d2519a76231fcaf503
DEEPAK-PC <dpakverma789@gmail.com>
2026-03-16 12:58:49 +0530

    fix

--- django_project_fixcart/urls.py
+++ django_project_fixcart/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fixcart_app.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
