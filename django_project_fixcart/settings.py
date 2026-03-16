c4c2fbe33b2a0e93159dbb65ed13f876aeb1d1d3
DEEPAK-PC <dpakverma789@gmail.com>
2026-03-16 13:25:49 +0530

    fix

--- django_project_fixcart/settings.py
+++ django_project_fixcart/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 MEDIA_URL = "/media/"
