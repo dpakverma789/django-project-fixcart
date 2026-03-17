0ea200dae6527705bc519dcd90a3e7c4c529e221
DEEPAK-PC <dpakverma789@gmail.com>
2026-03-17 16:29:43 +0530

    fix

--- fixcart_app/admin.py
+++ fixcart_app/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from .models import RepairRequest, RepairRequestImage
+from django.utils.html import format_html
 
 
 class RepairRequestImageInline(admin.TabularInline):
     model = RepairRequestImage
     extra = 1
+    # Show preview of uploaded image
+    readonly_fields = ("image_preview",)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="80" height="80" style="border-radius:5px;" />',
+                obj.image.url
+            )
+        return "No Image"
+
+    image_preview.short_description = "Preview"
 
 
 @admin.register(RepairRequest)
